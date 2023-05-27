58b8a4eec7d253b92cdfd4d9ef2f86bf3c2fdc8f
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2023-05-26 20:09:27 -0400

    Navmesh and OOP

--- Assets/Scripts/Enemy/EnemyMaster.cs
+++ Assets/Scripts/Enemy/EnemyMaster.cs
@@ -1,12 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyMaster : MonoBehaviour
+public interface IDamagable
 {
-    [SerializeField] private float health, speed;
-    private void OnTriggerEnter2D(Collider2D other)
+    void TakeDamage(float damage);
+}
+public class EnemyMaster : MonoBehaviour, IDamagable
+{
+    [SerializeField] private float health, speed, damage, damageInterval;
+    private NavMeshAgent agent;
+
+    private void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        Destroy(this.gameObject);
+    }
+    protected virtual void Movement()
+    {
+        Debug.Log("Hello");
     }
 }
--- Assets/Scripts/Weapons/SwordTestController.cs
+++ Assets/Scripts/Weapons/SwordTestController.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class SwordTestController : MonoBehaviour
 {
     private Animator animator;
+    [SerializeField] private float swordDamage;
     [HideInInspector] public bool inAnimation;
     private void Awake()
     {
         animator = this.GetComponent<Animator>();
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        IDamagable damagable = other.gameObject.GetComponent<IDamagable>();
+        if (damagable != null)
+        {
+            damagable.TakeDamage(swordDamage);
+        }
+    }
     public void HandleSwordSwingAnim(string animToPlay)
     {
         animator.SetTrigger(animToPlay);
