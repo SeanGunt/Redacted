e8628836558aea1dc6a0dda166c0b4143179b029
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2023-08-15 21:45:28 -0400

    Merge pull request #9 from SeanGunt/shopPlacement
    Add test sprite sheet, generate terrain from noise

--- Assets/Scripts/Map/MapUpdate.cs
+++ Assets/Scripts/Map/MapUpdate.cs
@@ -5,118 +5,169 @@ using UnityEngine.Tilemaps;
 
 public class MapUpdate : MonoBehaviour
 {
-    public TileBase[] tileVariants; // An array of your Tile Assets
+    /* SET FROM INSPECTOR */
 
-    private Tilemap tilemap;
+    // Density of rocks on the map
+    [Range(90, 100)]
+    public int rockDensity = 90;
 
-    public int tilemapHeight = 500, tilemapWidth = 500;
-    public int[,] map;
+    // The floor and ceiling of the float val that warrants grass
+    [Range(0.0f, 1.0f)]
+    public float grassFloor = 0.0f;
+    [Range(0.0f, 1.0f)]
+    public float grassCeiling = 0.0f;
+    
+    // The floor and ceiling of the float val that warrants dirt
+    [Range(0.0f, 1.0f)]
+    public float dirtFloor = 0.0f;
+    [Range(0.0f, 1.0f)]
+    public float dirtCeiling = 0.0f;
+
+    // The floor and ceiling of the float val that warrants water
+    [Range(0.0f, 1.0f)]
+    public float waterFloor = 0.0f;
+    [Range(0.0f, 1.0f)]
+    public float waterCeiling = 0.0f;
+
+    // An array of different tiles that we can use
+    public TileBase[] tileVariants;
+    public Vector3Int playerPos;
+    //public Material wavy;
+
+    // These variables are specific to internal functionality
+    private Tilemap tmBase, tmCollision;
+    readonly private float scale = 20f;
+    private float[,] noiseMap;  
+    readonly public int width = 500, height = 500;
+    private Dictionary<Vector3Int, List<TileBase>> layeredTiles = new Dictionary<Vector3Int, List<TileBase>>();
+
+    // Container for our tiles
+    struct Tile 
+    {
+        public int tileType;
+        public Vector3Int tilePos;
+    }
+    Tile[,] tiles;
 
     private void Awake()
     {
-        tilemap = GetComponent<Tilemap>();
+        // Get the tilemap
+        tmBase = CreateTilemap("BaseLayer");
+        tmCollision = CreateTilemap("CollisionLayer");
 
-        map = new int[tilemapWidth, tilemapHeight];
+        //tmBase.GetComponent<TilemapRenderer>().material = wavy;
+        tmCollision.gameObject.AddComponent<TilemapCollider2D>();
+
+        // Initialize noise map, and tile objects
+        noiseMap = new float[width, height];
+        tiles = new Tile[width, height];
+        int randomness = Random.Range(0, 10000);
         
-        if (map != null) GenerateLayout(ref map);
+        // Create the noise map
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(randomness + sampleX, randomness + sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        // Set an offset for placement and clear all the tiles if any are there.
+        const int offset = 250;
+        int counter = 0;
+        bool playerPosFound = false;
+
+        // Now loop and set the type and position of all tiles
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int variant = 0;
+                Vector3Int pos = new Vector3Int(x-offset, y-offset, 0);
+
+                if (noiseMap[x, y] > grassFloor && noiseMap[x, y] < grassCeiling) 
+                    variant = 1;
+
+                tiles[x, y].tileType = variant;
+                tiles[x, y].tilePos = pos;
+
+                if (!playerPosFound && x > 200 && x < 300 && y > 200 && y < 300)
+                {
+                    if (tiles[x, y].tileType == 1) counter++;
+
+                    if (counter > 5) {
+                        playerPos = pos;
+                        counter = 0;
+                        playerPosFound = true;
+                    }
+                }
+
+            }
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (x == 0 || y == 0 || y == height-1 || x == width-1) continue;
+                
+                int thisTile = tiles[x, y].tileType;
+                int leftTile = tiles[x-1, y].tileType;
+                int rightTile = tiles[x+1, y].tileType;
+                int topTile = tiles[x, y+1].tileType;
+                int botTile = tiles[x, y-1].tileType;
+
+                if (thisTile == 1 && leftTile == 0 && topTile == 0 && botTile != 0)
+                    tiles[x, y].tileType = 4;
+                else if (thisTile == 1 && botTile == 0 && rightTile == 0 && topTile != 0)
+                    tiles[x, y].tileType = 3;
+                else if (thisTile == 1 && topTile == 0 && rightTile == 0 && botTile != 0)
+                    tiles[x, y].tileType = 5;
+                else if (thisTile == 1 && leftTile == 0 && botTile == 0 && topTile != 0)
+                    tiles[x, y].tileType = 2;
+            }
+        }
     }
 
     private void Start()
     {
-        RenderMap(map, tilemap, tileVariants);
-    }
+        tmBase.ClearAllTiles();
+        tmCollision.ClearAllTiles();
 
-    private void Update()
-    {
-        // If screen has switched call GenerateLayout and then RenderMap
-    }
-
-    private void GenerateLayout(ref int[,] map)
-    {
-        for (int x = 0; x < tilemapWidth; x++)
+        // fill with primary tiles
+        for (int y = 0; y < height; y++)
         {
-            for (int y = 0; y < tilemapHeight; y++)
+            for (int x = 0; x < width; x++)
             {
-                // We are placing a grass block in every spot here for now
-                // soon we will have code here to randomly place trees, shops,
-                // roads, bushes, street lamps, stone statues, etc...
-                //
-                // The map is a 2D array of 1s for now, but we can use this
-                // loop to procedurally put numbers 1 - x where x is the number
-                // of possible tiles.
-                //
-                // I would like to have functions for each kind of object we can place
-                // they need to be able to select a different kind of block depending
-                // on the level so they shouldn't be too world specific... For instance,
-                // a palm tree instead of a pine tree.
-                //
-                // We can use a noise generation function to achieve grass and roads.
-                // We can find some kind of noise that looks similar to how trees would
-                // placed there. Then we can just follow where the roads are to place
-                // street lamps.
-                map[x,y] = 1;
+                if (tiles[x, y].tileType != 0)
+                    tmBase.SetTile(tiles[x, y].tilePos, tileVariants[tiles[x, y].tileType]);
+            }
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (tiles[x, y].tileType == 0)
+                    tmCollision.SetTile(tiles[x, y].tilePos, tileVariants[tiles[x, y].tileType]);
+
             }
         }
     }
 
-    private void RenderMap(int[,] map, Tilemap tilemap, TileBase[] tile)
+    Tilemap CreateTilemap(string tilemapName)
     {
-        const int offset = 250;
-        // Clear the map (ensures we don't overlap)
-        tilemap.ClearAllTiles();
-        
-        // Loop through the width of the map
-        for (int x = 0; x < map.GetUpperBound(0); x++)
-        {
-            // Loop through the height of the map
-            for (int y = 0; y < map.GetUpperBound(1); y++)
-            {
-                // 1 = tile, 0 = no tile
-                if (map[x, y] == 1)
-                {
-                    tilemap.SetTile(new Vector3Int(x-offset, y-offset, 0), tile[0]);
-                }
-            }
-        }
-    }
+        GameObject tilemapObject = new GameObject(tilemapName);
+        tilemapObject.transform.SetParent(transform); // Attach to the Grid
 
-/*
-    bool screenSwitched = true;
-    [SerializeField] TileBase[] tiles;
+        Tilemap tilemap = tilemapObject.AddComponent<Tilemap>();
+        tilemapObject.AddComponent<TilemapRenderer>();
 
-    void Awake()
-    {
-        // Get stuff ready here
+        return tilemap;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (screenSwitched)
-        {
-            PlaceTiles();
-            screenSwitched = false;
-        }
-    }
-
-    void GenerateGrid()
-    {
-        // TileBase[] tb = new TileBase[2];
-        // Resources.Load<TileBase>("Tilemaps/Tiles/level_" + level + "/tile_" + i);
-        Debug.Log("GenerateGrid() called");
-    }
-
-    void PlaceTiles()
-    {
-        int xAxis, yAxis;
-        for (xAxis = 0; xAxis < 10; xAxis++)
-        {
-            for (yAxis = 0; yAxis < 10; yAxis++)
-            {
-                // Put code here to place tiles from tile array
-                Debug.Log("PlaceTiles() called");
-            }
-        }
-    }
-*/
 }
+
