96a699bb9873221c0aa7dbc90af4dfc5719b5ff6
Jesse <77357219+jw209@users.noreply.github.com>
2023-08-20 16:09:23 -0400

    Add a test spritesheet and clean up method for generation

--- Assets/Scripts/Map/MapUpdate.cs
+++ Assets/Scripts/Map/MapUpdate.cs
@@ -1,35 +1,51 @@
 using System;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-enum Tiles
+enum LiquidTiles
 {
-    grass = 0,
-    water,
-    gravel
+    varA = 0
+}
+
+enum GroundTiles
+{
+    varA = 1,
+    varB,
+    varC,
+    botEdge,
+    leftEdge,
+    corner
+}
+
+enum PathTiles
+{
+    varA = 7,
+    varB,
+    varC
 }
 
 public class MapUpdate : MonoBehaviour
 {
-    [SerializeField] public TileBase[] tiles;
+    [SerializeField] public TileBase[] Tiles;
     [SerializeField] public float noiseScale;
     [SerializeField] public Vector2Int mapDimensions;
     [Range(-0.2f, 1.2f)]
-    [SerializeField] public float waterMin, waterMax;
+    [SerializeField] public float liquidMin, liquidMax;
     [Range(-0.2f, 1.2f)]
-    [SerializeField] public float dirtMin, dirtMax;
-    [Range(-0.2f, 1.2f)]
-    [SerializeField] public float grassMin, grassMax;
+    [SerializeField] public float groundMin, groundMax;
+    [SerializeField] public bool useOffset;
+    [SerializeField] public Vector2Int offset;
 
-    private Vector2Int offset;
     private Tilemap baseLayer, collisionLayer;
     private int[,] tileTypes;
 
     private void Awake()
     {   
-        offset.x = mapDimensions.x/2;
-        offset.y = mapDimensions.y/2;
+        if (!useOffset)
+        {
+            offset.x = mapDimensions.x/2;
+            offset.y = mapDimensions.y/2;   
+        } 
 
         baseLayer = CreateTilemap("base");
         collisionLayer = CreateTilemap("collision");
@@ -51,27 +67,15 @@ public class MapUpdate : MonoBehaviour
                 
                 float perlinValue = Mathf.PerlinNoise(rand + sampleX, rand + sampleY);
 
-                int type;
+                int type = (int)GroundTiles.varA;
                 if (x != 0 && x != mapDimensions.x-1 && y != 0 && y != mapDimensions.y-1)
-                    type = ChooseBaseType(perlinValue);
-                else
-                    type = (int)Tiles.grass;
+                    type = ChooseType(perlinValue, x, y);
 
                 tileTypes[x, y] = type;
             }
         }
 
-        for (int y = 0; y < mapDimensions.y; y++)
-        {
-            for (int x = 0; x < mapDimensions.x; x++)
-            {
-                if (x == 0 || x == mapDimensions.x-1 || y == 0 || y == mapDimensions.y-1)
-                    continue;
-                // this directly modifies the tileTypes[,] array
-                ChooseVariantType(x, y);
-            }
-        }
-
+        // Place base tiles
         for (int y = 0; y < mapDimensions.y; y++)
         {
             for (int x = 0; x < mapDimensions.x; x++)
@@ -79,6 +83,10 @@ public class MapUpdate : MonoBehaviour
                 PlaceTile(tileTypes[x, y], new Vector3Int(x-offset.x, y-offset.y, 0));
             }
         }
+
+        // Place roads
+
+        // Place prefab scenery
     }
 
     Tilemap CreateTilemap(string tilemapName)
@@ -94,49 +102,67 @@ public class MapUpdate : MonoBehaviour
 
     private void PlaceTile(int tileType, Vector3Int pos)
     {
-        if (tileType == (int)Tiles.water)
+        if (tileType == (int)LiquidTiles.varA)
         {
-            collisionLayer.SetTile(pos, tiles[tileType]);
+            collisionLayer.SetTile(pos, Tiles[tileType]);
         }
 
-        baseLayer.SetTile(pos, tiles[tileType]);
+        baseLayer.SetTile(pos, Tiles[tileType]);
     }
 
-    private int ChooseBaseType(float perlinValue)
+    private int ChooseType(float perlinValue, int Xindex, int Yindex)
     {
         int tileType = 0;
-        if (perlinValue > grassMin && perlinValue < grassMax) tileType = (int)Tiles.grass;
-        if (perlinValue > dirtMin && perlinValue < dirtMax) tileType = (int)Tiles.gravel;
-        if (perlinValue > waterMin && perlinValue < waterMax) tileType = (int)Tiles.water;
+        if (perlinValue > liquidMin && perlinValue < liquidMax)
+            tileType = ChooseVariantType((int)LiquidTiles.varA, Xindex, Yindex);
+        if (perlinValue > groundMin && perlinValue < groundMax)
+            tileType = ChooseVariantType((int)GroundTiles.varA, Xindex, Yindex);
 
         return tileType;
     }
 
-    private void ChooseVariantType(int x, int y)
-    {   
-        /*
-        if (tileTypes[x, y] == (int)Tiles.grass)
+    private int ChooseVariantType(int tileType, int Xindex, int Yindex)
+    {
+        int rand = UnityEngine.Random.Range(0, 100);
+        switch (tileType)
         {
-            // sides
-            int leftMostTile = tileTypes[x-1, y];
-            int botMostTile = tileTypes[x, y-1];
-            int topMostTile = tileTypes[x, y+1];
-            int rightMostTile = tileTypes[x+1, y];
+            case (int)GroundTiles.varA:
+                // choose a variant if random number is right
+                if (rand > 50)
+                {
+                    return (int)GroundTiles.varB;
+                }
+                else if (rand < 30)
+                {
+                    return (int)GroundTiles.varC;
+                }
 
-            if (leftMostTile == (int)Tiles.water)
-                tileTypes[x, y] = (int)Tiles.grass_edge_left;
-            if (botMostTile == (int)Tiles.water)
-                tileTypes[x, y] = (int)Tiles.grass_edge_bot;
-            if (rightMostTile == (int)Tiles.water)
-                tileTypes[x, y] = (int)Tiles.grass_edge_right;
+                // override variant decision with edge blocks if water is near
+                if (tileTypes[Xindex-1, Yindex] == (int)LiquidTiles.varA)
+                {
+                    return (int)GroundTiles.leftEdge;
+                }
+                else if (tileTypes[Xindex, Yindex-1] == (int)LiquidTiles.varA)
+                {
+                    return (int)GroundTiles.botEdge;
+                }
+                else if (tileTypes[Xindex-1, Yindex] == (int)GroundTiles.botEdge &&
+                    tileTypes[Xindex, Yindex-1] == (int)GroundTiles.leftEdge)
+                {
+                    return (int)GroundTiles.corner;
+                }
 
-            if (topMostTile == (int)Tiles.grass_edge_left && botMostTile == (int)Tiles.water)
-                tileTypes[x, y] = (int)Tiles.grass_edge_left;
-            if (topMostTile == (int)Tiles.grass_edge_right && botMostTile == (int)Tiles.water)
-                tileTypes[x, y] = (int)Tiles.grass_edge_right;
-            
+                break;
+            case (int)PathTiles.varA:
+                // path tile decisions here
+                break;
+            case (int)LiquidTiles.varA:
+                // liquid tile decisions here
+                return (int)LiquidTiles.varA;
+            default:
+                break;
         }
-        */
+        return (int)GroundTiles.varA;
     }
 }
 
