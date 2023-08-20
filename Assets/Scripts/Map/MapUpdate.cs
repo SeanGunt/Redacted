aa26853e6f446d52808fc5f0a32298f6a5bec1c6
Jesse <77357219+jw209@users.noreply.github.com>
2023-08-19 21:01:23 -0400

    Add new spritesheet and improve map update script

--- Assets/Scripts/Map/MapUpdate.cs
+++ Assets/Scripts/Map/MapUpdate.cs
@@ -1,173 +1,142 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+enum Tiles
+{
+    grass = 0,
+    water,
+    gravel
+}
+
 public class MapUpdate : MonoBehaviour
 {
-    /* SET FROM INSPECTOR */
+    [SerializeField] public TileBase[] tiles;
+    [SerializeField] public float noiseScale;
+    [SerializeField] public Vector2Int mapDimensions;
+    [Range(-0.2f, 1.2f)]
+    [SerializeField] public float waterMin, waterMax;
+    [Range(-0.2f, 1.2f)]
+    [SerializeField] public float dirtMin, dirtMax;
+    [Range(-0.2f, 1.2f)]
+    [SerializeField] public float grassMin, grassMax;
 
-    // Density of rocks on the map
-    [Range(90, 100)]
-    public int rockDensity = 90;
-
-    // The floor and ceiling of the float val that warrants grass
-    [Range(0.0f, 1.0f)]
-    public float grassFloor = 0.0f;
-    [Range(0.0f, 1.0f)]
-    public float grassCeiling = 0.0f;
-    
-    // The floor and ceiling of the float val that warrants dirt
-    [Range(0.0f, 1.0f)]
-    public float dirtFloor = 0.0f;
-    [Range(0.0f, 1.0f)]
-    public float dirtCeiling = 0.0f;
-
-    // The floor and ceiling of the float val that warrants water
-    [Range(0.0f, 1.0f)]
-    public float waterFloor = 0.0f;
-    [Range(0.0f, 1.0f)]
-    public float waterCeiling = 0.0f;
-
-    // An array of different tiles that we can use
-    public TileBase[] tileVariants;
-    public Vector3Int playerPos;
-    //public Material wavy;
-
-    // These variables are specific to internal functionality
-    private Tilemap tmBase, tmCollision;
-    readonly private float scale = 20f;
-    private float[,] noiseMap;  
-    readonly public int width = 500, height = 500;
-    private Dictionary<Vector3Int, List<TileBase>> layeredTiles = new Dictionary<Vector3Int, List<TileBase>>();
-
-    // Container for our tiles
-    struct Tile 
-    {
-        public int tileType;
-        public Vector3Int tilePos;
-    }
-    Tile[,] tiles;
+    private Vector2Int offset;
+    private Tilemap baseLayer, collisionLayer;
+    private int[,] tileTypes;
 
     private void Awake()
-    {
-        // Get the tilemap
-        tmBase = CreateTilemap("BaseLayer");
-        tmCollision = CreateTilemap("CollisionLayer");
+    {   
+        offset.x = mapDimensions.x/2;
+        offset.y = mapDimensions.y/2;
 
-        //tmBase.GetComponent<TilemapRenderer>().material = wavy;
-        //tmCollision.gameObject.AddComponent<TilemapCollider2D>();
+        baseLayer = CreateTilemap("base");
+        collisionLayer = CreateTilemap("collision");
+        collisionLayer.gameObject.AddComponent<TilemapCollider2D>();
 
-        // Initialize noise map, and tile objects
-        noiseMap = new float[width, height];
-        tiles = new Tile[width, height];
-        int randomness = Random.Range(0, 10000);
+        int rand = UnityEngine.Random.Range(0, 10000);
+        tileTypes = new int[mapDimensions.x, mapDimensions.y];
         
-        // Create the noise map
-        for (int y = 0; y < height; y++)
+        baseLayer.ClearAllTiles();
+        collisionLayer.ClearAllTiles();
+
+        // Get x by y grid of tiles from perlin noise
+        for (int y = 0; y < mapDimensions.y; y++)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < mapDimensions.x; x++)
             {
-                float sampleX = x / scale;
-                float sampleY = y / scale;
-
-                float perlinValue = Mathf.PerlinNoise(randomness + sampleX, randomness + sampleY);
-                noiseMap[x, y] = perlinValue;
-            }
-        }
-
-        // Set an offset for placement and clear all the tiles if any are there.
-        const int offset = 250;
-        int counter = 0;
-        bool playerPosFound = false;
-
-        // Now loop and set the type and position of all tiles
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                int variant = 0;
-                Vector3Int pos = new Vector3Int(x-offset, y-offset, 0);
-
-                if (noiseMap[x, y] > grassFloor && noiseMap[x, y] < grassCeiling) 
-                    variant = 1;
-
-                tiles[x, y].tileType = variant;
-                tiles[x, y].tilePos = pos;
-
-                if (!playerPosFound && x > 200 && x < 300 && y > 200 && y < 300)
-                {
-                    if (tiles[x, y].tileType == 1) counter++;
-
-                    if (counter > 5) {
-                        playerPos = pos;
-                        counter = 0;
-                        playerPosFound = true;
-                    }
-                }
-
-            }
-        }
-
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                if (x == 0 || y == 0 || y == height-1 || x == width-1) continue;
+                float sampleX = x / noiseScale;
+                float sampleY = y / noiseScale;
                 
-                int thisTile = tiles[x, y].tileType;
-                int leftTile = tiles[x-1, y].tileType;
-                int rightTile = tiles[x+1, y].tileType;
-                int topTile = tiles[x, y+1].tileType;
-                int botTile = tiles[x, y-1].tileType;
+                float perlinValue = Mathf.PerlinNoise(rand + sampleX, rand + sampleY);
 
-                if (thisTile == 1 && leftTile == 0 && topTile == 0 && botTile != 0)
-                    tiles[x, y].tileType = 4;
-                else if (thisTile == 1 && botTile == 0 && rightTile == 0 && topTile != 0)
-                    tiles[x, y].tileType = 3;
-                else if (thisTile == 1 && topTile == 0 && rightTile == 0 && botTile != 0)
-                    tiles[x, y].tileType = 5;
-                else if (thisTile == 1 && leftTile == 0 && botTile == 0 && topTile != 0)
-                    tiles[x, y].tileType = 2;
-            }
-        }
-    }
+                int type;
+                if (x != 0 && x != mapDimensions.x-1 && y != 0 && y != mapDimensions.y-1)
+                    type = ChooseBaseType(perlinValue);
+                else
+                    type = (int)Tiles.grass;
 
-    private void Start()
-    {
-        tmBase.ClearAllTiles();
-        tmCollision.ClearAllTiles();
-
-        // fill with primary tiles
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                if (tiles[x, y].tileType != 0)
-                    tmBase.SetTile(tiles[x, y].tilePos, tileVariants[tiles[x, y].tileType]);
+                tileTypes[x, y] = type;
             }
         }
 
-        for (int y = 0; y < height; y++)
+        for (int y = 0; y < mapDimensions.y; y++)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < mapDimensions.x; x++)
             {
-                if (tiles[x, y].tileType == 0)
-                    tmCollision.SetTile(tiles[x, y].tilePos, tileVariants[tiles[x, y].tileType]);
+                if (x == 0 || x == mapDimensions.x-1 || y == 0 || y == mapDimensions.y-1)
+                    continue;
+                // this directly modifies the tileTypes[,] array
+                ChooseVariantType(x, y);
+            }
+        }
 
+        for (int y = 0; y < mapDimensions.y; y++)
+        {
+            for (int x = 0; x < mapDimensions.x; x++)
+            {
+                PlaceTile(tileTypes[x, y], new Vector3Int(x-offset.x, y-offset.y, 0));
             }
         }
     }
 
     Tilemap CreateTilemap(string tilemapName)
     {
-        GameObject tilemapObject = new GameObject(tilemapName);
-        tilemapObject.transform.SetParent(transform); // Attach to the Grid
+        GameObject tilemapObject = new(tilemapName);
+        tilemapObject.transform.SetParent(transform);
 
         Tilemap tilemap = tilemapObject.AddComponent<Tilemap>();
         tilemapObject.AddComponent<TilemapRenderer>();
 
         return tilemap;
+    }
+
+    private void PlaceTile(int tileType, Vector3Int pos)
+    {
+        if (tileType == (int)Tiles.water)
+        {
+            collisionLayer.SetTile(pos, tiles[tileType]);
+        }
+
+        baseLayer.SetTile(pos, tiles[tileType]);
+    }
+
+    private int ChooseBaseType(float perlinValue)
+    {
+        int tileType = 0;
+        if (perlinValue > grassMin && perlinValue < grassMax) tileType = (int)Tiles.grass;
+        if (perlinValue > dirtMin && perlinValue < dirtMax) tileType = (int)Tiles.gravel;
+        if (perlinValue > waterMin && perlinValue < waterMax) tileType = (int)Tiles.water;
+
+        return tileType;
+    }
+
+    private void ChooseVariantType(int x, int y)
+    {   
+        /*
+        if (tileTypes[x, y] == (int)Tiles.grass)
+        {
+            // sides
+            int leftMostTile = tileTypes[x-1, y];
+            int botMostTile = tileTypes[x, y-1];
+            int topMostTile = tileTypes[x, y+1];
+            int rightMostTile = tileTypes[x+1, y];
+
+            if (leftMostTile == (int)Tiles.water)
+                tileTypes[x, y] = (int)Tiles.grass_edge_left;
+            if (botMostTile == (int)Tiles.water)
+                tileTypes[x, y] = (int)Tiles.grass_edge_bot;
+            if (rightMostTile == (int)Tiles.water)
+                tileTypes[x, y] = (int)Tiles.grass_edge_right;
+
+            if (topMostTile == (int)Tiles.grass_edge_left && botMostTile == (int)Tiles.water)
+                tileTypes[x, y] = (int)Tiles.grass_edge_left;
+            if (topMostTile == (int)Tiles.grass_edge_right && botMostTile == (int)Tiles.water)
+                tileTypes[x, y] = (int)Tiles.grass_edge_right;
+            
+        }
+        */
     }
 }
 
