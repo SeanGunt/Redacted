ddee75a22a89ca5f6f963672c15c4352242a2007
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2023-09-15 21:33:51 -0400

    Leveling

--- Assets/Scripts/Managers/AbilityManager.cs
+++ Assets/Scripts/Managers/AbilityManager.cs
@@ -1,41 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using TMPro;
-using System;
 
 public class AbilityManager : MonoBehaviour
 {
     private WeaponBase weaponBase;
+    private ExperienceManager experienceManager;
     private PlayerInput playerInput;
     private PlayerUI playerUI;
     public bool canLevelAbility;
-    private Dictionary<string, Action<int>> abilityLevelMap = new Dictionary<string, Action<int>>();
-    private List<string> abilityTypes = new List<string>();
+    public enum AbilityType
+    {
+        LevelQ, LevelE, LevelW, LevelR
+    }
+
     private void Awake()
     {
         weaponBase = GetComponentInChildren<WeaponBase>();
         playerInput = GetComponent<PlayerInput>();
         playerUI = GetComponent<PlayerUI>();
+        experienceManager = GetComponent<ExperienceManager>();
 
         canLevelAbility = true;
-
-        abilityLevelMap["LevelQ"] = (level) => { weaponBase.qLevel += level; };
-        abilityLevelMap["LevelW"] = (level) => { weaponBase.wLevel += level; };
-        abilityLevelMap["LevelE"] = (level) => { weaponBase.eLevel += level; };
-        abilityLevelMap["LevelR"] = (level) => { weaponBase.rLevel += level; };
-
-        abilityTypes.AddRange(abilityLevelMap.Keys);
     }
 
     private void Update()
     {
-        HandleAbilityLevelUI();
 
-        foreach (var abilityType in abilityTypes)
+        foreach (var abilityType in Enum.GetNames(typeof(AbilityType)))
         {
             HandleAbilityLeveling(abilityType);
+            HandleAbilityLevelingUI(abilityType);
         }
     }
 
@@ -47,22 +43,101 @@ public class AbilityManager : MonoBehaviour
         }
     }
 
+    private void HandleAbilityLevelingUI(string abilityType)
+    {
+        playerUI.levelIconsDict[abilityType].SetActive(IsUIForLevelActive(abilityType));
+    }
+
     private void LevelUpAbility(string abilityType)
     {
-        abilityLevelMap[abilityType](1);
-        canLevelAbility = false;
-    }    
+        int maxLevel = GetMaxAbilityLevel(abilityType);
+
+        switch (abilityType)
+        {
+            case "LevelQ":
+                if (weaponBase.qLevel < maxLevel)
+                {
+                    weaponBase.qLevel += 1;
+                    playerUI.qLevelText.text = weaponBase.qLevel.ToString();
+                    canLevelAbility = false;
+                }
+                break;
+            case "LevelW":
+                if (weaponBase.wLevel < maxLevel)
+                {
+                    weaponBase.wLevel += 1;
+                    playerUI.wLevelText.text = weaponBase.wLevel.ToString();
+                    canLevelAbility = false;
+                }
+                break;
+            case "LevelE":
+                if (weaponBase.eLevel < maxLevel)
+                {
+                    weaponBase.eLevel += 1;
+                    playerUI.eLevelText.text = weaponBase.eLevel.ToString();
+                    canLevelAbility = false;
+                }
+                break;
+            case "LevelR":
+                if (weaponBase.rLevel < maxLevel)
+                {
+                    weaponBase.rLevel += 1;
+                    playerUI.rLevelText.text = weaponBase.rLevel.ToString();
+                    canLevelAbility = false;
+                }
+                break;
+        }
+    }
 
     private bool CanLevelAbility(string abilityToLevel)
     {
-        return playerInput.actions[abilityToLevel].triggered && canLevelAbility;
+        int maxLevel = GetMaxAbilityLevel(abilityToLevel);
+
+        return playerInput.actions[abilityToLevel].triggered && canLevelAbility &&
+               GetAbilityLevel(abilityToLevel) < maxLevel &&
+               (abilityToLevel != "LevelR" || experienceManager.level >= maxLevel * 5 - 4);
     }
 
-    private void HandleAbilityLevelUI()
+    private bool IsUIForLevelActive(string abilityToLevel)
     {
-        playerUI.qLevelText.text = weaponBase.qLevel.ToString();
-        playerUI.wLevelText.text = weaponBase.wLevel.ToString();
-        playerUI.eLevelText.text = weaponBase.eLevel.ToString();
-        playerUI.rLevelText.text = weaponBase.rLevel.ToString();
+        int maxLevel = GetMaxAbilityLevel(abilityToLevel);
+
+        return canLevelAbility &&
+               GetAbilityLevel(abilityToLevel) < maxLevel &&
+               (abilityToLevel != "LevelR" || experienceManager.level >= maxLevel * 5 - 4);
+    }
+
+    private int GetMaxAbilityLevel(string abilityType)
+    {
+        if (abilityType == "LevelR")
+        {
+            if (experienceManager.level >= 16) return 3;
+            if (experienceManager.level >= 11) return 2;
+            if (experienceManager.level >= 6) return 1;
+        }
+        else
+        {
+            return 5;
+        }
+
+        return 0;
+    }
+
+    private int GetAbilityLevel(string abilityType)
+    {
+        switch (abilityType)
+        {
+            case "LevelQ":
+                return weaponBase.qLevel;
+            case "LevelW":
+                return weaponBase.wLevel;
+            case "LevelE":
+                return weaponBase.eLevel;
+            case "LevelR":
+                return weaponBase.rLevel;
+            default:
+                return 0;
+        }
     }
 }
+
---
+++ Assets/Scripts/Utility/DamageNumberHandler.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DamageNumberHandler : MonoBehaviour
+{
+    private Vector3 initialSize;
+    private TextMeshProUGUI numText;
+    private Color normalColor;
+    private Color critColor;
+    private void Awake()
+    {
+        initialSize = transform.localScale;
+        numText = GetComponent<TextMeshProUGUI>();
+        normalColor = numText.color;
+    }
+    private void OnEnable()
+    {
+        transform.localScale = initialSize;
+        numText.color = normalColor;
+        StartCoroutine(HandleShrink());
+    }
+
+    private IEnumerator HandleShrink()
+    {
+        Vector3 startSize = transform.localScale;
+        while (transform.localScale.x <= 0.015f)
+        {
+            startSize.x += Time.deltaTime * 0.025f;
+            startSize.y += Time.deltaTime * 0.025f;
+            transform.localScale = new Vector3(startSize.x, startSize.y, 0f);
+            yield return null;
+        }
+
+        StartCoroutine(HandleFade());
+
+        while (transform.localScale.x >= 0.005)
+        {
+            startSize.x -= Time.deltaTime * 0.01f;
+            startSize.y -= Time.deltaTime * 0.01f;
+            transform.localScale = new Vector3(startSize.x, startSize.y, 0f);
+            yield return null;
+        }
+    }
+
+    private IEnumerator HandleFade()
+    {
+        float startAlpha = 1f;
+        while (numText.color.a >= 0)
+        {
+            startAlpha -= Time.deltaTime;
+            numText.color = new Color(numText.color.r, numText.color.g, numText.color.b, startAlpha);
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+}
