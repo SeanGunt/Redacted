8af04a7734421546edf38c5635c4e74768d162c6
Jesse Wood <jessecodes98@gmail.com>
2023-11-12 20:32:45 -0500

    Set up wizard stuff

--- Assets/Scripts/Player/WizardGuy.cs
+++ Assets/Scripts/Player/WizardGuy.cs
@@ -7,7 +7,12 @@ public class WizardGuy : PlayerBase
     [SerializeField] private Staff staff;
     protected override void HandleQAbility() 
     {
-
+        if (CanUseAbility("QAttack", qCooldown, weaponBase.qLevel))
+        {
+            staff.abilityType = WeaponBase.AbilityType.Q;
+            staff.HandleStaffThrustAnim("Thrust");
+            StartCoroutine(HandleQCooldown());
+        }
     }
 
     protected override void HandleWAbility() 
--- Assets/Scripts/Weapons/Staff.cs
+++ Assets/Scripts/Weapons/Staff.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Staff : WeaponBase
 {
+    private Animator animator;
+    [HideInInspector] public BoxCollider2D weaponCollider;
+    [HideInInspector] public bool inAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        weaponCollider = GetComponent<BoxCollider2D>();
+        weaponCollider.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    #region Animation Events
+    public void HandleStaffThrustAnim(string animToPlay)
     {
-        
+        animator.SetTrigger(animToPlay);
     }
+
+    #endregion
 }
