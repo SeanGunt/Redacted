38dbf55c5399108d80735e5defcb1d710f8775a7
Jesse <77357219+jw209@users.noreply.github.com>
2023-11-20 21:45:53 -0500

    Set up damage for wizards Q ability

--- Assets/Scripts/Player/WizardGuy.cs
+++ Assets/Scripts/Player/WizardGuy.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class WizardGuy : PlayerBase
 {
     [SerializeField] private Staff staff;
+    private List<Transform> enemies = new List<Transform>();
     protected override void HandleQAbility() 
     {
         if (CanUseAbility("QAttack", qCooldown, weaponBase.qLevel))
         {
             staff.abilityType = WeaponBase.AbilityType.Q;
             staff.HandleStaffThrustAnim("Thrust");
+            SendBolt();
             StartCoroutine(HandleQCooldown());
         }
     }
@@ -28,5 +30,56 @@ public class WizardGuy : PlayerBase
     protected override void HandleRAbility() 
     {
 
+    }
+
+    private void SendBolt()
+    {
+        Vector3 playerPos = transform.position;
+        GetEnemiesOnScreen();
+        List<float> distances = CalculateDistances(playerPos);
+        SortEnemies(distances);
+        Vector3 targetPos = enemies[0].position;
+        IDamagable damagable = enemies[0].gameObject.GetComponent<IDamagable>();
+        damagable.TakeDamage(weaponBase.qDamage);
+    }
+
+    private void GetEnemiesOnScreen()
+    {
+        enemies.Clear();
+        GameObject[] enemyObjects = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemyObject in enemyObjects)
+        {
+            enemies.Add(enemyObject.transform);
+        }
+    }
+
+    private List<float> CalculateDistances(Vector3 playerPosition)
+    {
+        List<float> distances = new List<float>();
+        foreach (Transform enemy in enemies)
+        {
+            float distance = Vector3.Distance(playerPosition, enemy.position);
+            distances.Add(distance);
+        }
+        return distances;
+    }
+
+    private void SortEnemies(List<float> distances)
+    {
+        for (int i = 0; i <enemies.Count - 1; i++)
+        {
+            for (int j = i + 1; j <enemies.Count; j++)
+            {
+                int index1 = enemies.IndexOf(enemies[i]);
+                int index2 = enemies.IndexOf(enemies[j]);
+
+                if (distances[index1] > distances[index2])
+                {
+                    Transform temp = enemies[i];
+                    enemies[i] = enemies[j];
+                    enemies[j] = temp;
+                }
+            }
+        }
     }
 }
