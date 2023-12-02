28426f243bddea129db0055610077ff44764e575
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2023-12-01 22:37:50 -0500

    Items can now do things based on the invetory item child object, needs work

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InventorySlot : MonoBehaviour
 {
     [HideInInspector] public bool inventorySlotFilled;
+    [SerializeField] private string abilityBinding;
     private InventoryItem inventoryItem;
+    private PlayerInput playerInput;
 
-    private void Awake()
+    private void Start()
     {
-        inventoryItem = GetComponentInChildren<InventoryItem>();
+        playerInput = GameManager.Instance.player.GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        if (playerInput.actions[abilityBinding].triggered)
+        {
+            inventoryItem = GetComponentInChildren<InventoryItem>();
+            if (!inventoryItem.itemInitialized || inventoryItem == null) return;
+            
+        }
     }
 }
--- Assets/Scripts/Items/ItemBase.cs
+++ Assets/Scripts/Items/ItemBase.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
-public class ItemSO : ScriptableObject
+public class ItemBase : MonoBehaviour
 {
     public string itemName;
     public float health;
