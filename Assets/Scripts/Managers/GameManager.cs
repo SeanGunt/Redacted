e7c5702dbb454040c7901666a8cec359e8a976a0
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2025-01-04 20:32:36 -0500

    a

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using NavMeshPlus.Components;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
